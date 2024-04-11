a66809878ee159500376f304bebac5040a39ee2d
JOTALGS <jose.pgilsuarez@gmail.com>
2024-04-11 16:15:09 -0300

    styles

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,7 +2,6 @@
 """
 Flask web task 1
 """
-
 from flask import Flask
 app = Flask(__name__)
 
@@ -12,11 +11,11 @@ def index():
     """returns Hello"""
     return 'Hello HBNB!'
 
-
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
