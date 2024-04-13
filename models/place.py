d6ffa69b2f33e847907bfec273e8840dc97b700b
jotalgs <jose.pgilsuarez@gmail.com>
2024-04-12 21:27:30 -0300

    models

---
+++ models/place.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Place Module for HBNB project """
+from models.base_model import BaseModel, Base
+from models.review import Review
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship
+from os import getenv
+
+
+class Place(BaseModel, Base):
+    """ A place to stay """
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer(), default=0, nullable=False)
+    number_bathrooms = Column(Integer(), default=0, nullable=False)
+    max_guest = Column(Integer(), default=0, nullable=False)
+    price_by_night = Column(Integer(), default=0, nullable=False)
+    latitude = Column(Float(), nullable=True)
+    longitude = Column(Float(), nullable=True)
+    amenity_ids = []
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", backref="places", cascade="all, delete-orphan")
+    else:
+        @property
+        def reviews(self):
+            """Returns list, FileStorage relationship between Place and Review"""
+            from models import storage
+            my_list = []
+            for i in storage.all(Review):
+                if self.id == i.place_id:
+                    my_list.append(i)
+            return my_list
