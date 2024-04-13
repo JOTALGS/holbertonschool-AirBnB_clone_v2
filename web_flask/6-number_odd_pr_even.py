e1d273f7445068b352837ce6df1d7b67f56017fe
jotalgs <jose.pgilsuarez@gmail.com>
2024-04-12 22:13:19 -0300

    sa

---
+++ web_flask/6-number_odd_pr_even.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Flask web task 6
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """returns Hello"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def ctext(text):
+    """returns c text"""
+    text = text.replace("_", " ")
+    return 'C ' + text
+
+
+@app.route('/python', defaults={'text': "is_cool"})
+@app.route('/python/<text>')
+def pytext(text='is cool'):
+    """returns py text"""
+    text = text.replace("_", " ")
+    return 'Python  ' + text
+
+
+@app.route('/number/<int:n>')
+def isnumb(n):
+    """returns number int"""
+    return str(n) + ' is a number'
+
+
+@app.route('/number_template/<int:n>')
+def numbert(n):
+    """return template"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """return template with varr n"""
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port='5000')
